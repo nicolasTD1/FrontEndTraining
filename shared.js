329ac1e233f7786ae72e0e2faf712076cc3de13a
Nicolas Gonzalez <59267636+nicolasTD1@users.noreply.github.com>
2020-03-10 11:39:41 -0500

    7th commit
    Minor changes

--- shared.js
+++ shared.js
@@ -5,20 +5,16 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
-// console.log(backdrop);
-
 for(var i=0; i<selectPlanButtons.length; i++){
 	selectPlanButtons[i].addEventListener('click',function() {
 		// modal.style.display = 'block';
 		// backdrop.style.display = 'block';
-
 		modal.classList.add('open');
 		backdrop.classList.add('open');
 	});
 }
 
 backdrop.addEventListener("click", function(){
-	// mobileNav.style.display = 'none';
 	mobileNav.classList.remove('open');
 	closeModal();
 });
@@ -29,8 +25,6 @@ if(modalNoButton){
 
 
 function closeModal(){
-	// modal.style.display = 'none';
-	// backdrop.style.display = 'none';
 	if(modal){
 		modal.classList.remove('open');
 	}	
@@ -38,8 +32,6 @@ function closeModal(){
 }
 
 toggleButton.addEventListener("click", function(){
-	// mobileNav.style.display = 'block';
-	// backdrop.style.display = 'block';
 	mobileNav.classList.add('open');
 	backdrop.classList.add('open');
 
